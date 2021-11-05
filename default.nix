b12726a84a1d71195202be5eb81b81fe59b2eb12
Maciej Krüger <mkg20001@gmail.com>
2021-11-05 08:01:20 +0100

    fix: build

--- default.nix
+++ default.nix
@@ -7,8 +7,10 @@ stdenv.mkDerivation {
   src = ./.;
 
   installPhase = ''
+    sed "s|SELF=.*|SELF=$out|g" -i nixpkgs-container.sh
     install -D nixpkgs-container.sh $out/bin/lxd-nixpkgs-test
     install -D import.sh $out/bin/lxd-nixpkgs-test-import
+    cp -r shared inside $out
     ln -s $out/bin/lxd-nixpkgs-test $out/bin/lnt
   '';
 }
