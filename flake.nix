ed5fd7d864e08eccc8a44e6e8655ec2ea5e38cb1
Maciej Krüger <mkg20001@gmail.com>
2021-11-09 15:20:45 +0100

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Tiny script to help you test nixpkgs changes";
+
+  outputs = { self, nixpkgs }:
+
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+
+    {
+      overlay = (final: prev: {
+        lxd-nixpkgs-test = prev.callPackage ./. {};
+      });
+
+      defaultPackage = forAllSystems (system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      }).lxd-nixpkgs-test);
+
+    };
+}
+
